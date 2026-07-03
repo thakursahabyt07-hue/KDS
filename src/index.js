6ba82a73aed872fa547acf5139a0440a6376d800
thakursahabyt07-hue <thakursahab.yt07@gmail.com>
2026-07-04 01:25:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ export default {
       );
     }
 
-    const api = "https://aadhaar.asurpapa.workers.dev/api?key=Demo&aadhaar=${encodeURIComponent(aadhaar)};
+    const api = "https://aadhaar.asurpapa.workers.dev/api?key=Demo&aadhaar=${encodeURIComponent(aadhaar);
 
     try {
       const res = await fetch(api);
